0362c2c839e1a86553113a96b8e2c0cbfabf222c
Hung-Dev-Hutech <hung28122003cv@gmail.com>
2022-12-16 00:17:48 +0700

    Fix some array

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -1,14 +1,16 @@
 import sklearn 
 from sklearn import tree
+import numpy as np
 my_tree = tree.DecisionTreeClassifier()
-data_training=[[1,3,3,7],
+data_training = np.array([
+               [1,3,3,7],
                [5,2,4,6],
                [1,2,4,6],
                [5,4,4,3],
                [1,4,4,7],
                [3,2,3,7],
                [3,3,3,6],
-               [5,2,2,7]]
+               [5,2,2,7]])
 predicted_results =[0,1,1,0,0,0,0,1]
 result = my_tree.fit(data_training,predicted_results)
 person1 = [[1,4,3,3]]
